03bb00fc88342159b85cc06278bf7297b9000412
JEFF Liao <jeffliao92316046@gmail.com>
2023-02-26 18:18:03 +0800

    owo

--- BMI_1.java
+++ BMI_1.java
@@ -1,37 +1,38 @@
-import java.util.Scanner;
+import java.util.Scanner;//import scanner library
 
 public class BMI_1{
     public static void main(String[] args){
-        System.out.print("enter your height(cm) :");
-        Scanner h = new Scanner(System.in);
-        float height = h.nextFloat();
-        System.out.print("enter your weight(kg) :");
-        Scanner w = new Scanner(System.in);
-        float weight = w.nextFloat();
-        int flag = 1;
+        System.out.print("enter your height(cm) :"); //hint to input height  
+        Scanner h = new Scanner(System.in);// input height 
+        float height = h.nextFloat();//read height 
+        System.out.print("enter your weight(kg) :");//hint to input weight 
+        Scanner w = new Scanner(System.in);//input weight 
+        float weight = w.nextFloat();//read weight 
+        int flag = 1;//flag for judge height and weight rationality
         if (height > 300 || height < 0){
-            System.out.println("your height is irrational");
-            flag = 0;
+            System.out.println("your height is irrational");//output height is irrational
+            flag = 0;//judge height rationality
         } 
         if (weight > 400 || weight < 0){
-            System.out.println("your weight is irrational");
-            flag = 0;
+            System.out.println("your weight is irrational");//output weight is irrational
+            flag = 0;//judge weight rationality
         } 
-        float bmi = weight/((height*height)/(100*100));
+        float bmi = weight/((height*height)/(100*100));//calculate bmi
         if(flag == 1){
-            System.out.println("your bmi is : " + bmi);
+            System.out.println("your bmi is : " + bmi);//outputb bmi
+            //bmi suggestion
             if(bmi < 18.5){
-                System.out.println("you are Underweight");
+                System.out.println("you are Underweight\nyou should increase "+((18.5*height*height)/(100*100)-weight)+"kg");
             } else if (bmi < 24){
-                System.out.println("you are Normal");
+                System.out.println("you are Normal\nyou should keep your weight");
             } else if (bmi < 27){
-                System.out.println("you are Overweight(a little)");
+                System.out.println("you are Overweight(a little)\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg");
             } else if (bmi < 30){
-                System.out.println("you are Overweight(middle)");
+                System.out.println("you are Overweight(middle)\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg");
             } else if (bmi < 35){
-                System.out.println("you are Obese");
+                System.out.println("you are Obese\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg");
             } else {
-                System.out.println("you are Extremely Obese");
+                System.out.println("you are Extremely Obese\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg");
             }
         } 
         
--- BMI_2.java
+++ BMI_2.java
@@ -1,31 +1,32 @@
-import java.util.Scanner;
+import java.util.Scanner;//import scanner library
 
 public class BMI_2{
     public static void main(String[] args){
-        System.out.print("enter your height(cm) :");
-        Scanner h = new Scanner(System.in);
-        float height = h.nextFloat();
-        System.out.print("enter your weight(kg) :");
-        Scanner w = new Scanner(System.in);
-        float weight = w.nextFloat();
-        int flag = 1;
+        System.out.print("enter your height(cm) :"); //hint to input height  
+        Scanner h = new Scanner(System.in);// input height 
+        float height = h.nextFloat();//read height 
+        System.out.print("enter your weight(kg) :");//hint to input weight 
+        Scanner w = new Scanner(System.in);//input weight 
+        float weight = w.nextFloat();//read weight 
+        int flag = 1;//flag for judge height and weight rationality
         if (height > 300 || height < 0){
-            System.out.println("your height is irrational");
-            flag = 0;
+            System.out.println("your height is irrational");//output height is irrational
+            flag = 0;//judge height rationality
         } 
         if (weight > 400 || weight < 0){
-            System.out.println("your weight is irrational");
-            flag = 0;
+            System.out.println("your weight is irrational");//output weight is irrational
+            flag = 0;//judge weight rationality
         } 
-        float bmi = weight/((height*height)/(100*100));
+        float bmi = weight/((height*height)/(100*100));//calculate bmi
         if(flag == 1){
-            System.out.println("your bmi is : " + bmi);
-            System.out.println((bmi < 18.5) ? ("you are Underweight"):
-                                ((bmi < 24) ? ("you are Normal"):
-                                ((bmi < 27) ? ("you are Overweight(a little)"):
-                                ((bmi < 30) ? ("you are Overweight(middle)"):
-                                ((bmi < 35) ? ("you are Obese"):
-                                ("you are Extremely Obese"))))));
+            System.out.println("your bmi is : " + bmi);//outputb bmi
+            //bmi suggestion
+            System.out.println((bmi < 18.5) ? ("you are Underweight\nyou should increase "+((18.5*height*height)/(100*100)-weight)+"kg"):
+                                ((bmi < 24) ? ("you are Normal\nyou should keep your weight"):
+                                ((bmi < 27) ? ("you are Overweight(a little)\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg"):
+                                ((bmi < 30) ? ("you are Overweight(middle)\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg"):
+                                ((bmi < 35) ? ("you are Obese\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg"):
+                                ("you are Extremely Obese\nyou should decrease"+((24*height*height)/(100*100)-weight)+"kg"))))));
         }
     }
 }
