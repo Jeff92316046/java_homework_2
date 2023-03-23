0c44f2f6cf0a724c75ba04e26f056f16bcf60a4d
JEFF Liao <jeffliao92316046@gmail.com>
2023-03-23 08:44:36 +0800

    owo

---
+++ eng_test.java
@@ -0,0 +1,157 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.lang.Math;
+import java.lang.reflect.Type;
+
+public class eng_test {
+    public static void main(String[] args){
+        int question_num = 0;
+        int mode = 0;      
+        ArrayList<String> phrase = new ArrayList<String>(Arrays.asList("blow up","bring up","call off","do over","fill out","fill up","find out","give away","give back","hand in","hang up","hold up","leave out","look over","look up","make up","pick out","pick up","point out","put away"));
+        ArrayList<String> pharse_ts = new ArrayList<String>(Arrays.asList("爆炸","提起","取消","重複","填好","填滿","發現","贈送","歸還","繳交","懸掛","使延遲","省略","檢查","尋找","虛構","挑選","舉起","引起注意","儲蓄"));
+        //"blow up","bring up","call off","do over","fill out","fill up","find out","give away","give back","hand in","hang up","hold up","leave out","look over","look up","make up","pick out","pick up","point out","put away"	
+        //"爆炸","提起","取消","重複","填好","填滿","發現","贈送","歸還","繳交","懸掛","使延遲","省略","檢查","尋找","虛構","挑選","舉起","引起注意","儲蓄"
+        ArrayList<Integer> flag = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0));
+        ArrayList<Integer> flag_2 = new ArrayList<Integer>(Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0));
+        String a1_str = "";
+        String a2_str = "";
+        //ystem.out.println(rd);
+        
+        for(;;){
+            System.out.print("輸入題數n (n為-1時結束程式)(n>0):");
+            Scanner n = new Scanner(System.in);// input
+            question_num = n.nextInt();//read vaule
+            if(question_num<=0){
+                if(question_num==-1){
+                    break;
+                }else{
+                    System.out.println("輸入錯誤,請輸入大於等於2的正整數");
+                    continue;
+                }
+            }
+            System.out.println("輸入模式m\n純測驗模式m輸入1\n練習+提示模式輸入2");
+            Scanner m = new Scanner(System.in);// input
+            mode = m.nextInt();//read vaule
+            if(mode != 1 && mode != 2){
+                System.out.println("輸入錯誤,請輸入1或2");
+                continue;
+            }
+            int Max = phrase.size()-1;
+            int min = 0;
+            int rd = (int)(Math.random()*(Max-min+1))+min;
+            int point_counter = 0 ;
+            int correct_num = 0;
+            if(mode == 1){
+                System.out.println("");
+                for(int i=0;i<question_num;i++){
+                    for(int j=0;j<flag.size();j++){
+                        rd = (int)(Math.random()*(Max-min+1))+min;
+                        System.out.println(rd);
+                        if(flag.get(rd)==0){
+                            System.out.print(pharse_ts.get(rd)+" : ");
+                            flag.set(rd, flag.get(rd)+1);
+                            
+                            Scanner a1 = new Scanner(System.in);// input
+                            //Scanner a2 = new Scanner(System.in);// input
+                            a1_str = a1.nextLine();//read vaule
+                            //a2_str = a2.next();
+                            
+                            if(phrase.get(rd).equals(a1_str+""+a2_str)){
+                                System.out.println("正確");
+                                point_counter += 10 ;
+                                
+                            }else{
+                                System.out.println("錯誤");
+                            }
+                            break;
+                            
+                        }
+                    }
+                }
+                for(int i=0;i<flag.size();i++){
+                    flag.set(i,0);
+                }
+                System.out.println("你的分數 : " + point_counter + "/" + question_num*10);
+            }else if(mode == 2){
+                for(int first_check = 0;;first_check++){
+                    if(first_check == 0){
+                        for(int i=0;i<question_num;i++){
+                            for(int j=0;j<flag.size();j++){
+                                rd = (int)(Math.random()*(Max-min+1))+min;
+                                if(flag.get(rd)==0 && flag_2.get(rd)==0){
+                                    System.out.print(pharse_ts.get(rd)+" : ");
+                                    flag.set(rd, 1);
+                                    Scanner a1 = new Scanner(System.in);// input
+                                    //Scanner a2 = new Scanner(System.in);// input
+                                    a1_str = a1.nextLine();//read vaule
+                                    //a2_str = a2.next();
+                                    if(phrase.get(rd).equals(a1_str)){
+                                        System.out.println("正確");
+                                        point_counter += (10-first_check) ;
+                                        flag_2.set(rd, 1);
+                                        correct_num ++;
+                                    }else{
+                                        System.out.println("錯誤");
+                                        
+                                    }
+                                    break;
+                                }
+                            }
+                            
+                            
+                        }
+                        System.out.println("答對的總題數 : " + correct_num + "/" +question_num );
+                    }else{
+                        System.out.println("請問要繼續測驗嗎");
+                        System.out.println("輸入1結束本輪測驗\n輸入2繼續測驗");
+                        Scanner check = new Scanner(System.in);// input
+                        int c = check.nextInt();//read vaule
+                        if(c != 1 && c != 2){
+                            System.out.println("輸入錯誤,請輸入1或2");
+                            continue;
+                        }
+                        System.out.println("");
+                        if (c == 1)break;
+                        for(int i=0;i<question_num-correct_num;i++){
+                            for(int j=0;j<phrase.size();j++){
+                                if(flag.get(j)==0 && flag_2.get(j)==0){
+                                    System.out.print(pharse_ts.get(rd)+" : ");
+                                    flag.set(rd, 1);
+                                    Scanner a1 = new Scanner(System.in);// input
+                                    //Scanner a2 = new Scanner(System.in);// input
+                                    a1_str = a1.nextLine();//read vaule
+                                    //a2_str = a2.next();
+                                    if(phrase.get(rd).equals(a1_str)){
+                                        System.out.println("正確");
+                                        point_counter += (10-first_check) ;
+                                        flag_2.set(rd, 1);
+                                        correct_num ++;
+                                    }else{
+                                        System.out.println("錯誤");
+                                        
+                                    }
+                                    break;
+                                }
+                            }
+                            
+                            
+                        }
+                        System.out.println("答對的總題數 : " + correct_num + "/" +question_num );
+                    }
+                    
+                    if(correct_num == question_num)break;
+                    
+                    
+                }
+                System.out.println("你的分數 : " + point_counter + "/" + question_num*10);
+                for(int i=0;i<flag.size();i++){
+                    flag.set(i,0);
+                    flag_2.set(i, 0);
+                }
+            }
+
+        }
+        
+    }
+}
--- pyramid.java
+++ pyramid.java
@@ -1,112 +1,115 @@
-    import java.util.Scanner;
-    import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
-    public class pyramid { 
-        public static void main(String[] args){
-            do{                                         
-                int num = 0;
-                String str = "";
-                int choise = -1;
-                System.out.print("輸入 n (金字塔高度)(n為-1時結束程式)(n為奇數,n>=1):"); //hint to input 
-                try {
-                    Scanner n = new Scanner(System.in);// input
-                    num = n.nextInt();//read vaule
-                    if(num<=1 || num%2==0){
-                        if(num==-1){
-                            break;
-                        }else{
-                            System.out.println("輸入錯誤,請輸入大於等於1的奇數整數");
-                            continue;
-                        }
-                    }
-                }   catch(InputMismatchException ex){
-                    System.out.println("輸入錯誤,請輸入大於等於2的正整數");
-                    continue;
-                }
-                System.out.print("輸入 s (金字塔字元)(字數限制1)"); //hint to input 
-                try {
-                    Scanner s = new Scanner(System.in);// input
-                    str = s.next();//read vaule
-                    if(str.length()>1){
-                        System.out.println(str.length());
-                        System.out.println("輸入錯誤,請重新輸入");
+public class pyramid { 
+    public static void main(String[] args){
+        do{                                         
+            int num = 0;
+            String str = "abc";
+            int choise = -1;
+            System.out.print("輸入 n (金字塔高度)(n為-1時結束程式)(n為奇數,n>=1):"); //hint to input 
+            try{ 
+                Scanner n = new Scanner(System.in);// input
+                num = n.nextInt();//read vaule
+                if(num<=1 || num%2==0){
+                    if(num==-1){
+                        break;
+                    }else{
+                        System.out.println("輸入錯誤,請輸入大於等於1的奇數整數");
                         continue;
-                    
                     }
-                }   catch(InputMismatchException ex){
+                }
+            }   catch(InputMismatchException ex){
+                System.out.println("輸入錯誤,請輸入大於等於2的正整數");
+                continue;
+            }
+            System.out.print("輸入 s (金字塔字元)(字數限制1)"); //hint to input 
+            try {
+                Scanner s = new Scanner(System.in);// input
+                str = s.next();//read vaule
+                if(str.length()>1){
+                    System.out.println(str.length());
                     System.out.println("輸入錯誤,請重新輸入");
                     continue;
+                
                 }
-                System.out.print("選擇輸出形狀\nX輸入1\n*輸入2\nY輸入3\n"); //hint to input 
-                try {
-                    Scanner s = new Scanner(System.in);// input
-                    choise = s.nextInt();//read vaule
-                    if(choise != 1 && choise != 2 && choise != 3 ){
-                        System.out.println(choise);
-                        System.out.println("輸入錯誤,請重新輸入");
-                        continue;
-                    
-                    }
-                }   catch(InputMismatchException ex){
+            }   catch(InputMismatchException ex){
+                System.out.println("輸入錯誤,請重新輸入");
+                continue;
+            }
+            System.out.print("選擇輸出形狀\nX輸入1\n*輸入2\nY輸入3\n"); //hint to input 
+            try {
+                Scanner s = new Scanner(System.in);// input
+                choise = s.nextInt();//read vaule
+                if(choise != 1 && choise != 2 && choise != 3 ){
+                    System.out.println(choise);
                     System.out.println("輸入錯誤,請重新輸入");
                     continue;
+                
                 }
-                if(choise == 1){
-                    for(int i = 0;i < num;i++){ 
+            }   catch(InputMismatchException ex){
+                System.out.println("輸入錯誤,請重新輸入");
+                continue;
+            }
+            if(choise == 1){
+                for(int i = 0;i < num;i++){ 
+                    for(int j = 0;j < ((num-1)/2)-Math.abs(i-((num-1)/2)); j++){
+                        System.out.print(" ");     
+                    }
+                    if((num-1)/2 != i){
+                        System.out.print(str);  
+                    }
+                    for(int j = 0;j < 2*(Math.abs(i-((num-1)/2)))-1;j++){
+                        System.out.print(" ");
+                    }
+                    System.out.print(str);
+                    for(int j = 0;j < ((num-1)/2)-Math.abs(i-((num-1)/2)); j++){
+                        System.out.print(" ");     
+                    }
+                    System.out.print("\n"); 
+                }
+            }else if(choise == 2){
+                for(int i = 0;i < num;i++){
+                    if((num-1)/2 != i){
+                        //System.out.print(2-(Math.abs(i-((num-1)/2))+1));
                         for(int j = 0;j < ((num-1)/2)-Math.abs(i-((num-1)/2)); j++){
                             System.out.print(" ");     
                         }
-                        if((num-1)/2 != i){
-                            System.out.print(str);  
-                        }
+                        System.out.print(str);  
                         for(int j = 0;j < 2*(Math.abs(i-((num-1)/2)))-1;j++){
-                            System.out.print(" ");
-                        }
-                        System.out.print(str);
-                        for(int j = 0;j < ((num-1)/2)-Math.abs(i-((num-1)/2)); j++){
-                            System.out.print(" ");     
-                        }
-                        System.out.print("\n"); 
-                    }
-                }else if(choise == 2){
-                    for(int i = 0;i < num;i++){
-                        if((num-1)/2 != i){
-                            //System.out.print(2-(Math.abs(i-((num-1)/2))+1));
-                            for(int j = 0;j < ((num-1)/2)-Math.abs(i-((num-1)/2)); j++){
-                                System.out.print(" ");     
-                            }
-                            System.out.print(str);  
-                            for(int j = 0;j < 2*(Math.abs(i-((num-1)/2)))-1;j++){
-                                System.out.print(" ");
-                            }
-                            System.out.print(str);
-                            
-                            System.out.print("\n"); 
-                        }else{
-                            for(int j = 0;j < num;j++){
-                                System.out.print(str);
-                            }
-                            System.out.print("\n");
-                        }             
-                    }
-                }else{
-                    for(int i = 0;i < num;i++){
-                        for(int j = 0;j < i; j++){
-                            System.out.print(" ");     
-                        }
-                        if(i!=(num-1)){
-                            System.out.print(str);  
-                        }
-                        for(int j = 0;j < ((num-i-1)*2)-1;j++){
                             System.out.print(" ");
                         }
                         System.out.print(str);
                         
                         System.out.print("\n"); 
+                    }else{
+                        for(int j = 0;j < num;j++){
+                            System.out.print(str);
+                        }
+                        System.out.print("\n");
+                    }             
+                }
+            }else{
+                for(int i = 0;i < num;i++){
+                    for(int j = 0;j < i; j++){
+                        System.out.print(" ");     
                     }
-                } 
-                while(true)break;
-            }while(true);
-        }
-    }
+                    if(i!=(num-1)){
+                        System.out.print(str);  
+                    }
+                    for(int j = 0;j < ((num-i-1)*2)-1;j++){
+                        System.out.print(" ");
+                    }
+                    System.out.print(str);
+                    
+                    System.out.print("\n"); 
+                    
 
+                }
+            } 
+            while(true)break;
+        }while(true);
+    }
+}
+
+ 
